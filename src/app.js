128d401115f04dc039f6f2b9f8878d7d03f301c2
LDG <43874339+WellBeing-Man@users.noreply.github.com>
2021-11-16 15:52:32 +0900

    app.js Promise 코드 추가

--- src/app.js
+++ src/app.js
@@ -2,16 +2,26 @@ import { plus } from "./calculator.js";
 import "./app.css";
 import bg from "./bg.png";
 
-function answerDiv() {
+const answerDiv = () => {
   const div = document.createElement("div");
   div.innerText = `2+3의 답은 ${plus(2,3).toString()}입니다.`
   return div;
 }
 
-document
-  .getElementById("answer")
-  .appendChild(
-    answerDiv()
-  )
+const fetchBackground = () => {
+  return new Promise(function(resolve, reject){
+    setTimeout(function() {
+      resolve();
+    }, 2000);
+  })
+}
 
-document.body.style.backgroundImage = `url(${bg})`;
+document
+.getElementById("answer")
+.appendChild(
+  answerDiv()
+);
+
+fetchBackground().then(() => {
+  document.body.style.backgroundImage = `url(${bg})`;
+})
