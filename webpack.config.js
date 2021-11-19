682673fa574add302fd53ff368cc97aa9f024b07
LDG <43874339+WellBeing-Man@users.noreply.github.com>
2021-11-19 11:52:40 +0900

    todo

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,5 @@
 const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// todo plugin import
 
 
 module.exports = {
@@ -19,9 +16,9 @@ module.exports = {
       {
         test: /\.(css|scss)$/,
         use:[
-          process.env.NODE_ENV === "production" 
-          ? MiniCssExtractPlugin.loader
-          : "style-loader",
+          // todo css loader 설정
+          
+          "style-loader",
           "css-loader"
         ],
         sideEffects: true
@@ -37,35 +34,6 @@ module.exports = {
     ]
   },
   plugins:[
-    new webpack.BannerPlugin({
-      banner:
-      `프론트엔드의 세계에 오신 것을 완영합니다!!!
-      BuildDate: ${new Date().toLocaleDateString()}
-      Author: LDG  
-      `
-    }),
-
-    new CleanWebpackPlugin(),
-
-    ...( process.env.NODE_ENV === "production"
-    ? [new MiniCssExtractPlugin({filename: `[name].css`})]
-    : [] 
-    ),
-
-    new HtmlWebpackPlugin({
-      template:"./src/index.html",
-      templateParameters: {
-        env: process.env.NODE_ENV === "development" ? "개발용" : ""
-      },
-      minify:
-        process.env.NODE_ENV === "production"
-        ?{
-          collapseWhitespace: true,
-          removeComments: true
-        }
-        : false,
-      hash: process.env.NODE_ENV === "production"
-    }),
-
+      //todo plugins 설치 및 설정
   ]
 }
