3e709ae36d7ae966a1fb9c1b76a57177b4295415
LDG <43874339+WellBeing-Man@users.noreply.github.com>
2021-11-19 13:28:50 +0900

    todo

--- my-babel-plugin.js
+++ my-babel-plugin.js
@@ -1,10 +1,1 @@
-module.exports = function myPlugin() {
-  return {
-      visitor: {
-        Identifier(path) {
-          const name  = path.node.name ;
-          path.node.name = name.split("").reverse().join("");
-        }
-      }
-  }
-}
+//todo make your own plugin
